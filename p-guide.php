919ff8ea150a372397b6ff06aa554f04d7c437d6
kj65kawai <129245298+kj65kawai@users.noreply.github.com>
2023-03-30 15:44:08 +0900

    Add files via upload

---
+++ p-guide.php
@@ -0,0 +1,221 @@
+<!-- Copyright (c) 2009-2016, Kazuhisa Kawai, All rights reserved.	-->
+<!-- 									-->
+<!-- 	Public/P-Guide.php：「ＩＣＴ基礎ｃ」一般公開用講義の進め方	-->
+<!-- 									-->
+
+
+
+
+
+
+
+
+
+
+<html><head>
+<meta http-equiv="Content-type" content="text/html; charset=x-euc-jp">
+<title> Public Guide, Introduction to Information and Communication Technology (c), 16-H28, Kazuhisa J. Kawai.</title></head>
+<body bgcolor="#FFFFFF"text="#000000"vlink="#006600" link="#006600"><a name="top"><!-- Page Top --></a>
+<center>
+<h3>
+<a href="http://www.ita.cs.tut.ac.jp/~kawai/gi/public/">ＩＣＴ基礎ｃ</a>
+<br />
+<a href="http://www.cs.tut.ac.jp/">情報・知能工学系</a>
+&nbsp; <a href="http://www.ita.cs.tut.ac.jp/~kawai/index.html">河合 和久</a>
+</h3>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr align="center"><td bgcolor="#006633">
+<font color="#FFFFFF"><b>一般公開用講義の進め方&nbsp;<font size="-2">[ <a href="../en/p-guide.php"><font color="#FFFFFF">eng</font></a> | <a href=""><font color="#FFFFFF">jpn</font></a> ]</font> </b></font>
+</td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+</center>
+
+<center>
+<table width="80%"><tr><td>
+
+<p>
+　本講義では、下記のように講義と演習をくみあわせ、
+情報技術（ＩＣＴ）に関する基礎的概念の理解と基本的技術の習得を目標に、
+教科書「ネットワーク社会における情報の活用と技術（三訂版）」
+・「同学習ノート（三訂版）」の内容を順にすすめていく。
+同時に、
+教科書「Introduction to Information and Communication Technology.」の内容を、
+適宜とりあげていく。
+</p>
+
+<p>授業の構成<ul>
+<li> １回の授業で、おおむね教科書のひとつの節（1.1、1.2、…）をとりあげる。
+<li> 授業は、講義と演習を組み合わせてすすめていく。
+<li> 基本的には、９０分の授業時間のうち、 はじめの３０分間で、
+演習の発表・議論を行ない、 つづく６０分間で、
+その回の節のポイントとなる内容について講義を行なう。 
+</ul></p>
+
+<p>演習<ul>
+<li> 演習課題は、基本的に、教科書「学習ノート」にある問題である。
+<li> 演習では、各自、演習課題をレポートするとともに、発表担当者が発表する。
+発表に際しては、必ず ppt 資料を作成し、
+同資料を用いてプレゼンテーションを行なう。
+<li> 発表後、議論を行なう。
+<li> 発表に用いた ppt 資料は、
+ Moodle の学生プレゼンテーションにアップロードし、
+互いに参照できるようにする。
+<li> 例えば、教科書の1.1節をとりあげる回の演習では、
+受講生各自が、あらかじめ1.1節の演習課題を解き、
+そのレポートを授業開始時に提出し、発表担当者が授業の最初に発表を行なう。
+すなわち、演習のためのレポート作成・発表準備は、いわゆる「予習」にあたる。 
+</ul></p>
+
+<p>レポート<ul>
+<li> レポートは、あらかじめ用意されたフォーマットの Word 文書に、
+必要な内容を記入することによって作成し、その印刷したものを提出する。
+</ul></p>
+
+<p>講義<ul>
+<li> 講義では、演習で学習した内容を踏まえ、
+その回の節のポイントとなる内容・トピックをとりあげる。
+<li> 講義の最後に、ミニレポートを作成し、提出する。
+</ul></p>
+
+<p>Moodle<ul>
+<li> 本講義では、ｅラーニングシステムのひとつである
+ <a href="https://lms.imc.tut.ac.jp/">Moodle</A> を、
+プレゼンテーション資料や一部レポートの提出に用いる。
+</ul></p>
+
+<p>タイピング<ul>
+<li> タイピングは、もっとも基本的な情報技術のひとつである。
+本講義では、本学で作成したタイピング練習ソフトを使用して、
+タッチタイピング技術の習得を行なう。
+</ul></p>
+
+<p>成績<ul>
+<li> 受講状況（授業への参画度、プレゼンテーション、
+質疑応答、レポート、ミニレポート、タイピング）【５０％】、
+期末試験【５０％】をもとに、
+本講義の<a href="jugyou-shoukai.pdf">目標</a>を、
+どの程度達成したかを判定し、成績をつける。
+</ul></p>
+
+</td></tr></table>
+</center>
+
+<center>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="1" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="1" bgcolor="#009933">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr align="center"><td bgcolor="#009933">
+<font color="#FFFFFF"><b>スケジュール</b></font>
+</td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="1" bgcolor="#009933">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="1" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+</center>
+
+<center>
+<table WIDTH="80%"><tr><td>
+
+
+<p><table align=center cellspacing=0 cellpadding=4 border=1>
+<tr><td>Day#</td><td>月/日</td><td>内容</td><td>配付資料</td></tr>
+<tr><td>#1</td><td>04/13(Wed)</td>
+<td>ガイダンス／ＰＴ／アンケート／概要</td>
+<td align="center"><a href="../../private/jp/day01.php">◎</a></td></tr>
+<tr><td>#2</td><td>04/20(Wed)</td>
+<td>演習1・講義1（1.1：情報の概念）</td>
+<td align="center"><a href="../../private/jp/day02.php">◎</a></td></tr>
+<tr><td>#3</td><td>04/27(Wed)</td>
+<td>演習2・講義2（1.2：情報の収集・整理）</td>
+<td align="center"><a href="../../private/jp/day03.php">◎</a></td></tr>
+<tr><td>#4</td><td>05/11(Wed)</td>
+<td>演習3・講義3（1.3：情報の加工・表現）</td>
+<td align="center"><a href="../../private/jp/day04.php">◎</a></td></tr>
+<tr><td>#5</td><td>05/18(Wed)</td>
+<td>演習4・講義4（1.4：情報の発信・交換と評価）</td>
+<td align="center"><a href="../../private/jp/day05.php">◎</a></td></tr>
+<tr><td>#6</td><td>05/25(Wed)</td>
+<td>演習5・講義5（1.5：情報の管理とセキュリティ）</td>
+<td align="center"><a href="../../private/jp/day06.php">◎</a></td></tr>
+<tr><td>#7</td><td>06/01(Wed)</td>
+<td>演習6・講義6（2.1：問題解決の方法論）</td>
+<td align="center"><a href="../../private/jp/day07.php">◎</a></td></tr>
+<tr><td>#8</td><td>06/08(Wed)</td>
+<td>演習7・講義7（2.2,3：コンピュータと情報通信ネットワークのしくみ）</td>
+<td align="center"><a href="../../private/jp/day08.php">◎</a></td></tr>
+<tr><td>#9</td><td>06/15(Wed)</td>
+<td>演習8・講義8（2.4：情報のディジタル表現）</td>
+<td align="center"><a href="../../private/jp/day09.php">◎</a></td></tr>
+<tr><td>#10</td><td>06/22(Wed)</td>
+<td>演習9・講義9（2.5：コンピュータを利用した問題解決）</td>
+<td align="center"><a href="../../private/jp/day10.php">◎</a></td></tr>
+<tr><td>#11</td><td>06/29(Wed)</td>
+<td>演習10・講義10（2.6：セキュリティを守る技術）</td>
+<td align="center"><a href="../../private/jp/day11.php">◎</a></td></tr>
+<tr><td>#12</td><td>07/06(Wed)</td>
+<td>演習11・講義11（3.1：情報伝達の多様化と社会の変化）</td>
+<td align="center"><a href="../../private/jp/day12.php">◎</a></td></tr>
+<tr><td>#13</td><td>07/13(Wed)</td>
+<td>演習12・講義12（3.2：情報社会の進展）</td>
+<td align="center"><a href="../../private/jp/day13.php">◎</a></td></tr>
+<tr><td>#14</td><td>07/20(Wed)</td>
+<td>演習13・講義13（3.3：情報社会のもたらす影響と課題）</td>
+<td align="center"><a href="../../private/jp/day14.php">◎</a></td></tr>
+<tr><td>#15</td><td>07/27(Wed)</td>
+<td>演習14・講義14（3.4：情報社会における個人の役割と責任）</td>
+<td align="center"><a href="../../private/jp/day15.php">◎</a></td></tr>
+<tr><td>#16</td><td>08/09(Tue)<br />3rd Period<br />at A2-101</td>
+<td colspan =2>期末試験
+【日時・教室は他科目との調整により変更の可能性あり】
+いわゆる「何でも持ち込み可（ただし、いわゆる「機器」類＝例えば、
+電子辞書や電卓、ノートＰＣ、タブレット、ケータイなど、
+また、著しく大量のもの、を除く）」で行なう。</td></tr>
+</table></p>
+
+
+</td></tr></table>
+</center>
+
+<center>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr align="center"><td bgcolor="#006633">
+<font color="#FFFFFF"><b>２０１６（Ｈ２８）年度版
+&nbsp;<font size="-2">[ <a href="../en/p-guide.php"><font color="#FFFFFF">eng</font></a> | <a href=""><font color="#FFFFFF">jpn</font></a> ]</font> </b></font>
+</td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+
+<font size="-1">
+<a href="http://www.ita.cs.tut.ac.jp/~kawai/kawai/disclaimer.html">Copyright (c) 2009-2016, Kazuhisa Kawai, All Rights Reserved.</a><br />
+Please send comments to: <a href="mailto:kawai@tut.jp">kawai@tut.jp</a><br />
+Last updated April 12, 2016<br />
+Since November 30, 2009</font>
+</center>
+</body></html>
