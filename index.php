63dc2c142dee0a1fa9ab27cc2829abbe429f98d8
kj65kawai <129245298+kj65kawai@users.noreply.github.com>
2023-03-30 15:43:42 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!-- Copyright (c) 2009-2016, Kazuhisa Kawai, All rights reserved.	-->
+<!-- 									-->
+<!-- 	Public/JP/Index.php：「ＩＣＴ基礎ｃ」一般公開用講義資料目次	-->
+<!-- 									-->
+
+
+
+
+
+
+
+
+
+
+<html><head>
+<meta http-equiv="Content-type" content="text/html; charset=x-euc-jp">
+<title> Public Index, Introduction to Information and Communication Technology (c), 16-H28, Kazuhisa J. Kawai.</title></head>
+<body bgcolor="#FFFFFF"text="#000000"vlink="#006600" link="#006600"><a name="top"><!-- Page Top --></a>
+<center>
+<h3>
+<a href="http://www.ita.cs.tut.ac.jp/~kawai/gi/public/">ＩＣＴ基礎ｃ</a>
+<br />
+<a href="http://www.cs.tut.ac.jp/">情報・知能工学系</a>
+&nbsp; <a href="http://www.ita.cs.tut.ac.jp/~kawai/index.html">河合 和久</a>
+</h3>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr align="center"><td bgcolor="#006633">
+<font color="#FFFFFF"><b>一般公開用講義資料目次&nbsp;<font size="-2">[ <a href="../en/index.php"><font color="#FFFFFF">eng</font></a> | <a href=""><font color="#FFFFFF">jpn</font></a> ]</font> </b></font>
+</td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+</center>
+
+<center>
+<table width="80%">
+<tr valign="top"><td align="left"><b>　News: 　 </b></td>
+<td align="left">
+
+(160719) 第１５日（07/27）配付資料出来（<a
+ href="p-guide.php">講義の進め方</a>のスケジュール表）。
+受講生は、各自、必要な資料を印刷しておいてください。<br />
+(160719) 期末試験の日時は、８月９日（火）の３時限目に決まりました。
+これは、ＩＣＴ基礎「３クラス」一斉実施のためです。
+教室は、「A2-101」です。<br />
+<blockquote>
+　なお、試験はいわゆる「何でも持ち込み可（ただし、
+いわゆる「機器」類＝例えば、電子辞書や電卓、
+ノートＰＣ、タブレット、ケータイなど、
+また、著しく大量のもの、を除く。）」で行ないます。
+</blockquote>
+(160712) 第１４日（07/20）配付資料出来（<a
+ href="p-guide.php">講義の進め方</a>のスケジュール表）。
+受講生は、各自、必要な資料を印刷しておいてください。<br />
+
+<a href="prev-news.php"> News 一覧</a>
+</td></tr></table>
+</center>
+
+<center>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height=3 bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height=3 bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height=3 bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+</center>
+
+<center>
+<table width="80%"><tr><td>
+
+<ul>
+<li><a href="p-guide.php">講義の進め方</a>
+配付資料なども、このなかにあります。
+ただし、一部アクセス制限があります。
+<li> 教科書
+ <ul>
+ <li> 岡田正ほか：ネットワーク社会における情報の活用と技術（三訂版）
+ <li> 岡田正ほか：同学習ノート（三訂版）
+ <li> <a href="http://www.openbookproject.net/courses/intro2ict/index.html" target="_blank">Will Brady and Jeffrey Elkner: Introduction to Information and Communication Technology.</a>
+ </ul>
+<li><a href="jugyou-shoukai.pdf">シラバスより</a>
+<li><a href="results15.php">参考：過年度の成績分布</a>
+<li><a href="https://lms.imc.tut.ac.jp/"> Moodle へ</a>
+【コースカテゴリ：一般基礎】内にあります。
+<li><a href="/~kawai/kawai/schedule.html">オフィスアワー（水曜２時限・金曜２時限）</a>
+</ul>
+
+</td></tr></table>
+</center>
+
+<center>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr align="center"><td bgcolor="#006633">
+<font color="#FFFFFF"><b>２０１６（Ｈ２８）年度版
+&nbsp;<font size="-2">[ <a href="../en/index.php"><font color="#FFFFFF">eng</font></a> | <a href=""><font color="#FFFFFF">jpn</font></a> ]</font> </b></font>
+</td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#006633">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+<table width="90%" cellpadding="0" cellspacing="0" border="0">
+<tr><td height="3" bgcolor="#FFFFFF">
+<img src="1pixel.gif" width="1" height="1" border="0"></td></tr></table>
+
+<font size="-1">
+<a href="http://www.ita.cs.tut.ac.jp/~kawai/kawai/disclaimer.html">Copyright (c) 2009-2016, Kazuhisa Kawai, All Rights Reserved.</a><br />
+Please send comments to: <a href="mailto:kawai@tut.jp">kawai@tut.jp</a><br />
+Last updated July 19, 2016<br />
+Since November 10, 2009</font>
+</center>
+</body></html>
